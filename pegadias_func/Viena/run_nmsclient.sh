c45635a1f7444ab84bfdca3297650af822dac80f
arthursmacedo <arthur.2siqueira@gmail.com>
2024-04-17 12:03:29 -0300

    Remove some files

--- pegadias_func/Viena/run_nmsclient.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-nms_client.sh I01AR_2023_04_23.req -f I01AR_2023_04_23.mseed
-nms_client.sh I11CV_2023_04_23.req -f I11CV_2023_04_23.mseed
-nms_client.sh I42PT_2023_04_23.req -f I42PT_2023_04_23.mseed
-nms_client.sh I37NO_2023_04_23.req -f I37NO_2023_04_23.mseed
-nms_client.sh I56US_2023_04_23.req -f I56US_2023_04_23.mseed
